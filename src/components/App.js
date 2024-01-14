7abd36a6ac542babc111dfe791e862e39bbc8ef2
UtsavKumar126/ <guptautsavkumar126@gmail.com>
2024-01-14 17:20:58 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,27 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
+  const[data,setData]=useState("");
+  useEffect(()=>{
+    fetchData();
+  },[])
+  
+  async function fetchData(){
+
+    const response= await fetch("https://dummyjson.com/products");
+    const data= await response.json();
+
+    setData(data);
+
+  }
+    return (
     <div>
         {/* Do not remove the main div */}
+        {
+          !data?<p>Loading....</p>:<pre>{JSON.stringify(data)}</pre>
+        }
     </div>
   )
 }
