ea8d1a4945f44d92855890df142f2a8f1cb53edf
UtsavKumar126/ <guptautsavkumar126@gmail.com>
2024-01-14 17:29:49 +0530

    forth

--- src/components/App.js
+++ src/components/App.js
@@ -13,18 +13,18 @@ const App = () => {
     try{
     const response= await fetch("https://dummyjson.com/products");
     const data= await response.json();
-
     setData(data);
     }
     catch(error){
       setData(error);
+      console.log(error);
     }
   }
     return (
     <div>
         {/* Do not remove the main div */}
         {
-          !data?<h1>Loading...</h1>:<pre>{JSON.stringify(data)}</pre>
+          !data?<h1>Loading...</h1>:<h1><pre>{JSON.stringify(data)}</pre></h1>
         }
     </div>
   )
